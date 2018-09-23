8f3f4df716fec2a127d0ec80c7667b93283799e9
kpcyrd <git@rxv.cc>
2018-09-23 03:55:08 +0200

    Split into bin and lib

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,19 @@
-#![allow(proc_macro_derive_resolution_fallback)]
-extern crate rustyline;
+#![warn(unused_extern_crates)]
+extern crate sn0int;
 extern crate env_logger;
-extern crate rand;
-extern crate colored;
 #[macro_use] extern crate failure;
-extern crate shellwords;
-extern crate dirs;
-extern crate publicsuffix;
-extern crate chrootable_https;
-extern crate trust_dns_proto;
-extern crate url;
-extern crate hlua_badtouch as hlua;
-extern crate hyper;
-extern crate base64;
-extern crate http;
-extern crate kuchiki;
-extern crate serde;
-extern crate serde_json;
-extern crate serde_urlencoded;
-#[macro_use] extern crate serde_derive;
-#[macro_use] extern crate log;
-#[macro_use] extern crate nom;
-#[macro_use] extern crate structopt;
-#[macro_use] extern crate lazy_static;
-#[macro_use] extern crate diesel;
-#[macro_use] extern crate diesel_migrations;
-#[macro_use] extern crate crossbeam_channel as channel;
+extern crate structopt;
 
-pub mod args;
-pub mod cmd;
-pub mod complete;
-pub mod db;
-pub mod errors;
-pub mod engine;
-pub mod html;
-pub mod json;
-pub mod migrations;
-pub mod models;
-pub mod paths;
-pub mod ser;
-pub mod schema;
-pub mod shell;
-pub mod runtime;
-pub mod term;
-pub mod web;
-pub mod worker;
-pub mod psl;
-pub mod utils;
-
-use args::{Args, SubCommand};
-use errors::*;
-use engine::Module;
+use sn0int::args::{self, Args, SubCommand};
+use sn0int::cmd;
+use sn0int::errors::*;
+use sn0int::engine::{self, Module};
+use sn0int::shell;
 use structopt::StructOpt;
 use std::path::Path;
 
 
-fn run_run(gargs: &args::Args, args: &args::Run) -> Result<()> {
+fn run_run(gargs: &Args, args: &args::Run) -> Result<()> {
     let mut rl = shell::init(gargs)?;
 
     if let Some(module) = &args.module {
