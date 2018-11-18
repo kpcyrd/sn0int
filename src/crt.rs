7bc7030e5c9d978e882fd7cb78b0952a652bde23
kpcyrd <git@rxv.cc>
2018-11-18 08:33:45 +0100

    Accept ip addresses in san extension parser

--- src/crt.rs
+++ src/crt.rs
@@ -3,10 +3,11 @@ use errors::*;
 use x509_parser;
 use der_parser::oid::Oid;
 use std::collections::HashSet;
+use std::net::{Ipv4Addr, Ipv6Addr};
 use nom::be_u8;
 
 
-named!(san_extension<&[u8], Result<Vec<String>>>, do_parse!(
+named!(san_extension<&[u8], Vec<String>>, do_parse!(
     _tag:   tag!(b"\x30")   >>
     len:    be_u8           >>
     values: take!(len)      >>
@@ -15,22 +16,57 @@ named!(san_extension<&[u8], Result<Vec<String>>>, do_parse!(
         let mut values = Vec::new();
         while !bytes.is_empty() {
             let (rem, v) = san_value(bytes)?;
-            values.push(v);
+            match v {
+                Ok(v) => values.push(v),
+                Err(e) => warn!("Unknown field in SAN extension: {}", e),
+            }
             bytes = rem;
         }
-
-        values.into_iter()
-            .collect()
+        values
     })
 ));
 
-named!(san_value<&[u8], Result<String>>, do_parse!(
-    _tag:   tag!(b"\x82")   >>
+named!(san_value<&[u8], Result<String>>,
+    switch!(be_u8,
+        0x82    => call!(san_value_dns) |
+        0x87    => call!(san_value_ipaddr) |
+        k       => call!(san_value_unknown, k)
+    )
+);
+
+named!(san_value_dns<&[u8], Result<String>>, do_parse!(
     len:    be_u8           >>
     value:  take!(len)      >>
     ({
         String::from_utf8(value.to_vec())
             .map_err(Error::from)
+    })
+));
+
+named!(san_value_ipaddr<&[u8], Result<String>>, do_parse!(
+    len:    be_u8           >>
+    v:      take!(len)      >>
+    ({
+        match len {
+            4  => Ok(Ipv4Addr::from([
+                v[0], v[1], v[2], v[3],
+            ]).to_string()),
+            16 => Ok(Ipv6Addr::from([
+                v[0],  v[1],  v[2],  v[3],
+                v[4],  v[5],  v[6],  v[7],
+                v[8],  v[9],  v[10], v[11],
+                v[12], v[13], v[14], v[15],
+            ]).to_string()),
+            _ => Err(format_err!("Invalid ipaddr")),
+        }
+    })
+));
+
+named_args!(san_value_unknown(key: u8)<&[u8], Result<String>>, do_parse!(
+    len:    be_u8           >>
+    v:      take!(len)      >>
+    ({
+        Err(format_err!("Unexpected type {:?} => {:?}", key, v))
     })
 ));
 
@@ -92,12 +128,13 @@ impl Certificate {
                 continue;
             }
 
+            debug!("Found san extension: {:?}", x.value);
             let values = match san_extension(x.value) {
                 Ok((remaining, values)) => {
                     if !remaining.is_empty() {
                         bail!("san extension has trailing garbage");
                     }
-                    values?
+                    values
                 },
                 Err(_) => bail!("Failed to parse san extension"),
             };
@@ -117,7 +154,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parse_pem() {
+    fn test_parse_pem_github() {
         let mut x = Certificate::parse_pem(r#"-----BEGIN CERTIFICATE-----
 MIIHQjCCBiqgAwIBAgIQCgYwQn9bvO1pVzllk7ZFHzANBgkqhkiG9w0BAQsFADB1
 MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3
@@ -167,13 +204,52 @@ WUjbST4VXmdaol7uzFMojA4zkxQDZAvF5XgJlAFadfySna/teik=
     }
 
     #[test]
+    fn test_parse_pem_1_1_1_1() {
+        let mut x = Certificate::parse_pem(r#"-----BEGIN CERTIFICATE-----
+MIID9DCCA3qgAwIBAgIQBWzetBRl/ycHFsBukRYuGTAKBggqhkjOPQQDAjBMMQsw
+CQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMSYwJAYDVQQDEx1EaWdp
+Q2VydCBFQ0MgU2VjdXJlIFNlcnZlciBDQTAeFw0xODAzMzAwMDAwMDBaFw0yMDAz
+MjUxMjAwMDBaMGwxCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMN
+U2FuIEZyYW5jaXNjbzEZMBcGA1UEChMQQ2xvdWRmbGFyZSwgSW5jLjEdMBsGA1UE
+AwwUKi5jbG91ZGZsYXJlLWRucy5jb20wWTATBgcqhkjOPQIBBggqhkjOPQMBBwNC
+AASyRQsxrFBjziHmfDQjGsXBU0WWl3oxh7vg6h2V9f8lBMp18PY/td9R6VvJPa20
+AwVzIJI+dL6OSxviaIZEbmK7o4ICHDCCAhgwHwYDVR0jBBgwFoAUo53mH/naOU/A
+buiRy5Wl2jHiCp8wHQYDVR0OBBYEFN+XTeVDs7BBp0LykM+Jf64SV4ThMGMGA1Ud
+EQRcMFqCFCouY2xvdWRmbGFyZS1kbnMuY29thwQBAQEBhwQBAAABghJjbG91ZGZs
+YXJlLWRucy5jb22HECYGRwBHAAAAAAAAAAAAERGHECYGRwBHAAAAAAAAAAAAEAEw
+DgYDVR0PAQH/BAQDAgeAMB0GA1UdJQQWMBQGCCsGAQUFBwMBBggrBgEFBQcDAjBp
+BgNVHR8EYjBgMC6gLKAqhihodHRwOi8vY3JsMy5kaWdpY2VydC5jb20vc3NjYS1l
+Y2MtZzEuY3JsMC6gLKAqhihodHRwOi8vY3JsNC5kaWdpY2VydC5jb20vc3NjYS1l
+Y2MtZzEuY3JsMEwGA1UdIARFMEMwNwYJYIZIAYb9bAEBMCowKAYIKwYBBQUHAgEW
+HGh0dHBzOi8vd3d3LmRpZ2ljZXJ0LmNvbS9DUFMwCAYGZ4EMAQICMHsGCCsGAQUF
+BwEBBG8wbTAkBggrBgEFBQcwAYYYaHR0cDovL29jc3AuZGlnaWNlcnQuY29tMEUG
+CCsGAQUFBzAChjlodHRwOi8vY2FjZXJ0cy5kaWdpY2VydC5jb20vRGlnaUNlcnRF
+Q0NTZWN1cmVTZXJ2ZXJDQS5jcnQwDAYDVR0TAQH/BAIwADAKBggqhkjOPQQDAgNo
+ADBlAjEAjoyy2Ogh1i1/Kh9+psMc1OChlQIvQF6AkojZS8yliar6m8q5nqC3qe0h
+HR0fExwLAjAueWRnHX4QJ9loqMhsPk3NB0Cs0mStsNDNG6/DpCYw7XmjoG3y1LS7
+ZkZZmqNn2Q8=
+-----END CERTIFICATE-----
+"#).expect("Failed to parse cert");
+        x.valid_names.sort();
+        assert_eq!(x, Certificate {
+            valid_names: vec![
+                "*.cloudflare-dns.com".into(),
+                "1.0.0.1".into(),
+                "1.1.1.1".into(),
+                "2606:4700:4700::1001".into(),
+                "2606:4700:4700::1111".into(),
+                "cloudflare-dns.com".into(),
+            ],
+        });
+    }
+
+    #[test]
     fn test_san_extension() {
         let (rem, ext) = san_extension(&[48, 28,
                 130, 10, 103, 105, 116, 104, 117, 98, 46, 99, 111, 109,
                 130, 14, 119, 119, 119, 46, 103, 105, 116, 104, 117, 98, 46, 99, 111, 109
             ])
             .expect("Failed to parse extension");
-        let ext = ext.expect("Extension contains invalid data");
         assert!(rem.is_empty());
         assert_eq!(ext, vec![
             String::from("github.com"),
