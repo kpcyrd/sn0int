52db46c34055f41b1e4b6ca913de296b27a174f3
kpcyrd <git@rxv.cc>
2018-11-25 00:37:20 +0100

    Adjust sandbox for arm

--- src/sandbox/seccomp.rs
+++ src/sandbox/seccomp.rs
@@ -11,8 +11,11 @@ pub fn init() -> Result<()> {
     ctx.allow_syscall(Syscall::sigaltstack)?;
     ctx.allow_syscall(Syscall::munmap)?;
     ctx.allow_syscall(Syscall::openat)?;
+    #[cfg(not(target_arch = "aarch64"))]
     ctx.allow_syscall(Syscall::open)?;
     ctx.allow_syscall(Syscall::fcntl)?;
+    #[cfg(target_arch = "arm")]
+    ctx.allow_syscall(Syscall::fcntl64)?;
     ctx.allow_syscall(Syscall::uname)?;
     ctx.allow_syscall(Syscall::close)?;
     ctx.allow_syscall(Syscall::epoll_create1)?;
@@ -21,15 +24,23 @@ pub fn init() -> Result<()> {
     ctx.allow_syscall(Syscall::sched_getaffinity)?;
     ctx.allow_syscall(Syscall::socket)?;
     ctx.allow_syscall(Syscall::connect)?;
+    #[cfg(not(target_arch = "aarch64"))]
     ctx.allow_syscall(Syscall::epoll_wait)?;
     ctx.allow_syscall(Syscall::epoll_pwait)?;
     ctx.allow_syscall(Syscall::getrandom)?;
     ctx.allow_syscall(Syscall::bind)?;
     ctx.allow_syscall(Syscall::ioctl)?;
+    #[cfg(target_arch = "arm")]
+    ctx.allow_syscall(Syscall::send)?;
     ctx.allow_syscall(Syscall::sendto)?;
+    #[cfg(target_arch = "arm")]
+    ctx.allow_syscall(Syscall::recv)?;
     ctx.allow_syscall(Syscall::recvfrom)?;
     ctx.allow_syscall(Syscall::getsockopt)?;
+    #[cfg(not(target_arch = "arm"))]
     ctx.allow_syscall(Syscall::mmap)?;
+    #[cfg(target_arch = "arm")]
+    ctx.allow_syscall(Syscall::mmap2)?;
     ctx.allow_syscall(Syscall::mprotect)?;
     ctx.allow_syscall(Syscall::clone)?;
     ctx.allow_syscall(Syscall::set_robust_list)?;
