795688ecc943b4c94da193de2cd8327f965e1b74
kpcyrd <git@rxv.cc>
2018-12-07 18:13:52 +0100

    Add poc arp-scan parser

---
+++ modules/dev/arp-scan.lua
@@ -0,0 +1,21 @@
+-- Description: Parse arp-scan output
+-- Version: 0.1.0
+-- License: GPL-3.0
+
+-- sudo arp-scan -qglI wlp3s0
+
+function run()
+    while true do
+        x = stdin_readline()
+        if x == nil then
+            break
+        end
+
+        m = regex_find('(.+)\t(.+)', x)
+        if m ~= nil then
+            ip = m[2]
+            mac = m[3]
+            info(json_encode({ip, mac}))
+        end
+    end
+end
