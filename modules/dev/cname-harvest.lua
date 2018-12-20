ffc8ce6a3c094d46eee43601375eff2e21f6d88e
kpcyrd <git@rxv.cc>
2018-12-20 20:43:33 +0100

    Bump some module versions

--- modules/dev/cname-harvest.lua
+++ modules/dev/cname-harvest.lua
@@ -1,5 +1,5 @@
 -- Description: Query for CNAMES to find subdomains
--- Version: 0.1.0
+-- Version: 0.2.0
 -- Source: subdomains
 -- License: GPL-3.0
 
--- modules/dev/ctlogs.lua
+++ modules/dev/ctlogs.lua
@@ -1,5 +1,5 @@
 -- Description: Query certificate transparency logs to discover subdomains
--- Version: 0.1.0
+-- Version: 0.2.0
 -- Source: domains
 -- License: GPL-3.0
 
