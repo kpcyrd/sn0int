28865968939983ca9cfae3b6dce4a3f55561b268
kpcyrd <git@rxv.cc>
2019-02-28 05:11:26 +0100

    Add tests for json functions

--- src/runtime/json.rs
+++ src/runtime/json.rs
@@ -25,3 +25,48 @@ pub fn json_decode_stream(lua: &mut hlua::Lua, state: Arc<State>) {
             .map_err(|err| state.set_error(err))
     }))
 }
+
+
+#[cfg(test)]
+mod tests {
+    use crate::engine::ctx::Script;
+
+    #[test]
+    fn verify_json_encode() {
+        let script = Script::load_unchecked(r#"
+        function run()
+            x = json_encode({1,2,3,4})
+            if x ~= '[1,2,3,4]' then
+                return 'encode failed: ' .. x
+            end
+        end
+        "#).expect("Failed to load script");
+        script.test().expect("Script failed");
+    }
+
+    #[test]
+    fn verify_json_decode() {
+        let script = Script::load_unchecked(r#"
+        function run()
+            x = json_decode('[1,2,3,4]')
+            if not (x[1] == 1 and x[2] == 2 and x[3] == 3 and x[4] == 4) then
+                return 'decode failed: ' .. json_encode(x)
+            end
+        end
+        "#).expect("Failed to load script");
+        script.test().expect("Script failed");
+    }
+
+    #[test]
+    fn verify_json_decode_stream() {
+        let script = Script::load_unchecked(r#"
+        function run()
+            x = json_decode_stream('[1,2][3,4]')
+            if not (x[1][1] == 1 and x[1][2] == 2 and x[2][1] == 3 and x[2][2] == 4) then
+                return 'decode_stream failed: ' .. json_encode(x)
+            end
+        end
+        "#).expect("Failed to load script");
+        script.test().expect("Script failed");
+    }
+}
