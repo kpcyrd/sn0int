9ed6cf8993a0195d566a26a184183eeeefb97714
kpcyrd <git@rxv.cc>
2019-06-09 01:36:04 +0200

    Fix bugs in pgp-keyserver.lua

--- modules/dev/pgp-keyserver.lua
+++ modules/dev/pgp-keyserver.lua
@@ -4,48 +4,66 @@
 -- License: GPL-3.0
 
 function run(arg)
-    session = http_mksession()
+    local domain = arg['value']
 
     --lookup_url = 'https://pgp.mit.edu/pks/lookup'
-    lookup_url = 'https://sks-keyservers.net/pks/lookup'
+    local lookup_url = 'https://sks-keyservers.net/pks/lookup'
 
-    req = http_request(session, 'GET', lookup_url, {
+    session = http_mksession()
+    local req = http_request(session, 'GET', lookup_url, {
         query={
-            search=arg['value'],
+            search=domain,
         }
     })
 
-    resp = http_send(req)
+    local resp = http_send(req)
     if last_err() then return end
     if resp['status'] ~= 200 then return 'http error: ' .. resp['status'] end
 
     links = html_select_list(resp['text'], 'a')
+    debug('keys found: ' .. #links)
     for i=1, #links do
         href = links[i]['attrs']['href']
 
         if href:find('/pks/lookup%?op=get&search=') == 1 then
             url = url_join(lookup_url, href)
 
+            debug(url)
             req = http_request(session, 'GET', url, {})
-
             resp = http_send(req)
             -- TODO: do not abort script if one attempt fails
             if last_err() then return end
-            if resp['status'] ~= 200 then return 'http error: ' .. resp['status'] end
 
-            pubkey = pgp_pubkey_armored(resp['text'])
+            if resp['status'] ~= 200 then
+                error('http error: ' .. url .. ' => ' .. resp['status'])
+            else
+                pubkey = pgp_pubkey_armored(resp['text'])
+                -- print(pubkey)
 
-            -- print(pubkey)
+                emails = {}
+                domain_matched = false
 
-            -- TODO: ensure at least one email matches our target domain
-            if pubkey['uids'] then
-                for j=1, #pubkey['uids'] do
-                    local m = regex_find("(.+) <([^< ]+@[^< ]+)>$", pubkey['uids'][j])
-                    if m then
-                        db_add('email', {
-                            value=m[3],
-                            displayname=m[2],
-                        })
+                -- TODO: ensure at least one email matches our target domain
+                if pubkey['uids'] then
+                    for j=1, #pubkey['uids'] do
+                        local m = regex_find("(.+) <([^< ]+@[^< ]+)>$", pubkey['uids'][j])
+                        if m then
+                            local email = m[3]
+                            emails[#emails+1] = {
+                                value=email,
+                                displayname=m[2],
+                            }
+
+                            if email:match('@' .. domain .. '$') then
+                                domain_matched = true
+                            end
+                        end
+                    end
+                end
+
+                if domain_matched then
+                    for j=1, #emails do
+                        db_add('email', emails[j])
                     end
                 end
             end
