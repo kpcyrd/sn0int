1da35e4e88bd31da50b659734a87560733aec00d
kpcyrd <git@rxv.cc>
2019-09-29 09:12:33 +0200

    Move modules to github.com/kpcyrd/sn0int-modules

--- modules/dev/images.lua
+++
@@ -1,9 +0,0 @@
--- Description: Parse image metadata
--- Version: 0.1.0
--- License: GPL-3.0
--- Source: images
-
-function run(arg)
-    local img = img_load(arg['value'])
-    db_update('image', arg, img)
-end
