5fc97140f344d18090cd8faecc842d4cbb3abe62
kpcyrd <git@rxv.cc>
2020-02-29 01:36:38 +0100

    Point to online docs on first start

--- src/shell/mod.rs
+++ src/shell/mod.rs
@@ -507,6 +507,7 @@ pub fn init<'a>(args: &Args, config: &'a Config, verbose_init: bool) -> Result<S
 
     if verbose_init && library.list().is_empty() {
         term::success("No modules found, run \x1b[1mpkg quickstart\x1b[0m to install default modules");
+        term::success("New to sn0int? Follow https://sn0int.rtfd.io/en/stable/usage.html");
     }
 
     let autoupdate = AutoUpdater::load()?;
